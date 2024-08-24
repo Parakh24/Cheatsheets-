acf3174dd9491d9b6a22b36ff7a44a34e89aee2d
Parakh <parakhvirnawe2404@gmail.com>
2024-08-24 10:50:21 +0530

    Two files tests/main_test.py and src/main.py are added

---
+++ src/main.py
@@ -0,0 +1,24 @@
+
+#!/usr/bin/env python3
+#test for factorial 
+
+def factorial(number) -> int:
+ if number==0 or number==1:
+    return 1
+ 
+ 
+ else:
+    return number*factorial(number-1)  
+       
+
+
+
+
+def main():
+   print(f"Factorial: {factorial(5)}") 
+   print(f"Factorial: {factorial(4)}")
+
+
+if __name__ == "__main__":
+   main()  
+    
